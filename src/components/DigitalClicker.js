c7b2ead0b776cb8bea42b993629457708e82b4bb
Alison Golden <alisong779@yahoo.com>
2019-09-05 14:54:38 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,24 @@
-// Code DigitalClicker Component Here
+import React from 'react'
+
+export default class DigitalClicker extends React.Component {
+  // This component has an initial state property called timesClicked, which is initially defined as 0.
+  constructor(){
+    super()
+    this.state = {
+      timesClicked: 0
+    }
+  }
+// Whenever the button is clicked, update the state by incrementing the timesClicked by 1
+  incrementClick = () => {
+    const clickCount = this.state.timesClicked + 1
+    this.setState({
+      timesClicked: clickCount
+    })
+  }
+// The component renders out a button with a label that shows the timesClicked value.
+  render(){
+    return (
+      <button onClick={this.incrementClick}>{this.state.timesClicked}</button>
+    )
+  }
+}
